90630ef686c280bcbff951310f13f646fd9bdf9d
Lukas Preisinger <preisinger@stund.fri.sk>
2024-04-01 17:32:21 +0200

    Prvý commit. Len základná štruktúra programu pre začiatok.

---
+++ app/src/main/java/com/example/tetris_bloky_v_pohybe/MainActivity.kt
@@ -0,0 +1,30 @@
+package com.example.tetris_bloky_v_pohybe
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import android.view.View
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.tetris_bloky_v_pohybe.ui.theme.Tetris_bloky_v_pohybeTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+
+    fun ukoncenie(view: View?){
+        finishAndRemoveTask()
+    }
+
+    fun start(view: View?){
+
+    }
+}
